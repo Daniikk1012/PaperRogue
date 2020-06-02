c814a6fbb3be750a959b9ddccb9df31289c48cec
daniikk1012 <forlessons.12@gmail.com>
2020-06-02 12:13:16 +0600

    danik is danik

---
+++ desktop/src/com/wgsoft/game/paperrogue/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wgsoft.game.paperrogue.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wgsoft.game.paperrogue.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
