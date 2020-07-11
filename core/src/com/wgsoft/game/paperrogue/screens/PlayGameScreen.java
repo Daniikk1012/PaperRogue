e0a9f288d616b25068ed9aae70a5064cb080da2a
daniikk1012 <forlessons.12@gmail.com>
2020-07-11 21:58:44 +0600

    lol

--- core/src/com/wgsoft/game/paperrogue/screens/PlayGameScreen.java
+++ core/src/com/wgsoft/game/paperrogue/screens/PlayGameScreen.java
@@ -1,5 +1,6 @@
 package com.wgsoft.game.paperrogue.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -10,5 +11,27 @@ public class PlayGameScreen {
 
     public PlayGameScreen() {
         stage = new Stage(new ScreenViewport(), game.batch);
+    }
+    @Override
+    public void render(float delta) {
+        stage.act(delta);
+        stage.draw();
+    }
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+    @Override
+    public void resize(int width, int height) {
+        if((float)width/height > 800f/480f){
+            ((ScreenViewport)stage.getViewport()).setUnitsPerPixel(480f/height);
+        }else{
+            ((ScreenViewport)stage.getViewport()).setUnitsPerPixel(800f/width);
+        }
+        stage.getViewport().update(width, height, true);
+    }
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
     }
 }
