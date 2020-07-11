c0530d9e900fbb4d0a9fd41043b1d8f5de2ecf95
daniikk1012 <forlessons.12@gmail.com>
2020-07-11 21:55:57 +0600

    gg

--- core/src/com/wgsoft/game/paperrogue/screens/PlayGameScreen.java
+++ core/src/com/wgsoft/game/paperrogue/screens/PlayGameScreen.java
@@ -1,4 +1,14 @@
 package com.wgsoft.game.paperrogue.screens;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import static com.wgsoft.game.paperrogue.MyGdxGame.game;
+
 public class PlayGameScreen {
+    private Stage stage;
+
+    public PlayGameScreen() {
+        stage = new Stage(new ScreenViewport(), game.batch);
+    }
 }
