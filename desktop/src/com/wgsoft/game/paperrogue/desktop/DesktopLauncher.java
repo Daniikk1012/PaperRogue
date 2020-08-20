b18ca5a265f64dab277a817590845f6fd3709c28
Daniikk-1012 <daniyarsar@gmail.com>
2020-08-20 16:11:17 +0600

    Update DesktopLauncher.java

--- desktop/src/com/wgsoft/game/paperrogue/desktop/DesktopLauncher.java
+++ desktop/src/com/wgsoft/game/paperrogue/desktop/DesktopLauncher.java
@@ -6,11 +6,10 @@ import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.wgsoft.game.paperrogue.MyGdxGame;
 
 public class DesktopLauncher {
-	private static final boolean PACK = true;
 	public static void main (String[] arg) {
 		TexturePacker.Settings settings = new TexturePacker.Settings();
 		settings.combineSubdirectories = true;
-		TexturePacker.process(settings, "raw/img", "img", "skin");
+		TexturePacker.processIfModified(settings, "raw/img", "img", "skin");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.forceExit = false;
 		new LwjglApplication(new MyGdxGame(), config);
